42c4458c0ef3196161ce6d52de3f9648f9a18b54
robert <velascorobert1820@gmail.com>
2026-05-13 14:33:10 -0500

    Pequeño ajuste en refMoto

--- src/main/java/com/dilanmotos/infrastructure/persistence/ReferenciaMotoJpaRepository.java
+++ src/main/java/com/dilanmotos/infrastructure/persistence/ReferenciaMotoJpaRepository.java
@@ -2,19 +2,8 @@ package com.dilanmotos.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.dilanmotos.domain.model.ReferenciaMoto;
-
-import java.util.*;
 
 // Debe extender de ReferenciaEntity porque así se llama tu clase anotada con @Entity
 public interface ReferenciaMotoJpaRepository extends JpaRepository<ReferenciaEntity, Integer> {
-    
-    ReferenciaMoto guardar(ReferenciaMoto ReferenciaMoto);
 
-    List<ReferenciaMoto> obtenerTodos();
-
-    Optional<ReferenciaMoto> buscarPorId(int id);
-
-    void eliminarPorId(int id);
-    
 }
