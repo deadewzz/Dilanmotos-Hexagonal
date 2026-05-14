aca9a0306c7b8ae12f768fb1e9426580ace579a5
Diver Mauricio <diver1402@gmail.com>
2026-05-14 15:45:11 -0500

    Correcion login de usuarios

--- src/main/java/com/dilanmotos/infrastructure/controller/UsuarioController.java
+++ src/main/java/com/dilanmotos/infrastructure/controller/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/usuarios/login")
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
