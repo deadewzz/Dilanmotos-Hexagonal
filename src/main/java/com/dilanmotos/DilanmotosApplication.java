1ea3ff8e4a047c847f1ccc1cb3440f4f18eedc29
Deadewzz <Manrique.neyder3011@gmail.com>
2026-07-27 20:58:03 -0500

    Creación de variables de entorno

--- src/main/java/com/dilanmotos/DilanmotosApplication.java
+++ src/main/java/com/dilanmotos/DilanmotosApplication.java
@@ -5,6 +5,7 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
@@ -16,6 +17,12 @@ public class DilanmotosApplication {
     }
 
     public static void main(String[] args) {
+        // Carga el archivo .env antes de arrancar la aplicación de Spring
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(DilanmotosApplication.class, args);
     }
 
